adb65178f50bd7dc3e8b2ffb20eec1290a706998
s2d <aseprizal70@gmail.com>
2019-10-24 11:31:40 +0700

    init data

--- app/home/homeController.js
+++ app/home/homeController.js
@@ -1,13 +1,26 @@
 var app = angular.module('myApp');
 app.controller('HomeController', function PhoneList($scope, $http) {
-	$http.get('http://api.aladhan.com/v1/calendar?latitude=51.508515&longitude=-0.1254872&method=2&month=4&year=2017').then(
-		function successCallback(response) {
-        	$scope.greeting = response.data.data;
-        },
-        function errorCallback(response) {
-        	alert("Unable to perform get request");
-      	}
-    );
+    $scope.loadFeeds = function () {
+        $http.get('http://api.aladhan.com/v1/calendar?latitude=51.508515&longitude=-0.1254872&method=2&month=4&year=2017').then(
+            function successCallback(response) {
+                $scope.greeting = response.data.data;
+            },
+            function errorCallback(response) {
+                alert("Unable to perform get request");
+            }
+        );
+    };
+    $scope.loadFeeds();//you have leave this line pf code
+	
+  //   $http.get('http://api.aladhan.com/v1/calendar?latitude=51.508515&longitude=-0.1254872&method=2&month=4&year=2017').then(
+		// function successCallback(response) {
+  //       	$scope.greeting = response.data.data;
+  //       },
+  //       function errorCallback(response) {
+  //       	alert("Unable to perform get request");
+  //     	}
+  //   );
+    
     $http.post('https://apifactory.telkom.co.id:8243/hcm/auth/v1/token', {
 		"username": "402256",
 		"password": "Sflozi14"
